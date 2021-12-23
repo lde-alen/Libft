99f7e1bb5585c9c5d54010097bd878017941c648
Lucas De alencastro moreira <lde-alen@lab1r3s7.42abudhabi.ae>
2021-12-23 17:37:03 +0400

    libft final push

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: lde-alen <lde-alen@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 11:40:39 by lde-alen          #+#    #+#             */
-/*   Updated: 2021/12/23 13:29:25 by lde-alen         ###   ########.fr       */
+/*   Updated: 2021/12/23 17:34:40 by lde-alen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,16 @@
  */
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t			i;
-	const char		*tmp_src;
-	char			*tmp_dest;
+	size_t					i;
+	unsigned const char		*tmp_src;
+	unsigned char			*tmp_dest;
 
-	tmp_src = src;
-	tmp_dest = dst;
-	i = 1;
+	tmp_src = (unsigned const char *)src;
+	tmp_dest = (unsigned char *)dst;
+	i = 0;
 	if (!(tmp_dest || tmp_src))
 		return (NULL);
-	while (i <= n)
+	while (i < n)
 	{
 		*tmp_dest = *tmp_src;
 		tmp_dest++;
